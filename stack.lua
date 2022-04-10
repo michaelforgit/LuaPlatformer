f7c7da42312ab4dc60151349a090f828fc180880
michaelforgit <michaelforemail@protonmail.com>
2022-04-09 20:15:25 -0500

    made UIStack -> stack so when using non UI objects it looks cleaner

---
+++ stack.lua
@@ -0,0 +1,19 @@
+Stack = Object.extend(Object);
+
+local stackArr = {}; 
+
+function Stack:new()
+    self.stackArr = {}
+end
+
+function Stack:push(obj)
+    table.insert(self.stackArr, obj)
+end
+
+function Stack:pop()
+    table.remove(self.stackArr, #self.stackArr);
+end
+
+function Stack:top() 
+    return (self.stackArr[#self.stackArr]);
+end
