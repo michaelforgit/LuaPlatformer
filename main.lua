5f0f6070a18ca5f94255492678009abc9ac11824
michaelforgit <michaelforemail@protonmail.com>
2022-04-09 20:16:11 -0500

    reworked to work with new file names, made the window  resizable and exitable with the esc key

--- main.lua
+++ main.lua
@@ -8,7 +8,8 @@ function love.load()
     require "player";
     require "bullet";
     require "enemy";
-    require "uistack"
+    require "stack";
+    require "uiobject";
     wf = require "windfield";
     love.physics.setMeter(10);
     world = wf.newWorld(0, 300, false);
@@ -17,23 +18,24 @@ function love.load()
     world:addCollisionClass("Floor");
     world:addCollisionClass("Enemy");
     world:addCollisionClass("Bullet");
-    love.window.setFullscreen(true);
+    love.window.setMode(800, 600, {resizable = true});
 
-    font = love.graphics.newFont("Abaddon_Fonts_v1.2/Abaddon Light.ttf", 20)
+    font = love.graphics.newFont("Abaddon_Fonts_v1.2/Abaddon Light.ttf", 20);
     love.graphics.setFont(font);
 
     player = Player();
-    enemys = {}
-    platforms = {}
+    enemys = {};
+    platforms = {};
     enemy = Enemy(120, 224.00);
     table.insert(enemys, enemy);
-    guiObjects = UiStack();
+    guiStack = Stack();
     local test = {love.graphics.getWidth()/2-love.graphics.getWidth()/8, 
     love.graphics.getHeight()-love.graphics.getHeight()/3.5, 
     love.graphics.getWidth()/4, 
     love.graphics.getHeight()/4, 
+    35,
     "The quick brown fox jumped over the fence.  I do not know the rest this is just a test font."}
-    guiObjects:push(test);
+    guiStack:push(UIRectangle(test));
 
     if gameMap.layers["floor"] then
         for i, obj in pairs(gameMap.layers["floor"].objects) do
@@ -61,54 +63,13 @@ function love.load()
                 --print(player.collider:getPosition());
             end
         end
-    end)
-    --[[enemy.collider:setPostSolve(function(collider_1, collider_2, contact)
-        if collider_1.collision_class == "Enemy" and collider_2.collision_class == "Bullet" then
-            enemy.health = enemy.health - 5;
-            print(enemy.health);
-            for i,v in pairs(listOfBullets) do
-                if v.collider == collider_2 then
-                    v.collider:destroy();
-                    table.remove(listOfBullets, i);
-                end
-            end
-            if enemy.health == 0 then
-                for i,v in pairs(enemys) do
-                    if v == enemy then
-                        table.remove(enemys, i);
-                        print("HERE");
-                    end
-                end
-                enemy.collider:destroy();
-            end
-        end
-    end)]]
-    
-end
-
-function createPopUp(specs)
-    local x = specs[1];
-    local y = specs[2];
-    local width = specs[3];
-    local height = specs[4];
-    local closeSize = 35;
-    local text = specs[5];
-    love.graphics.setColor(1, 1, 1);
-    love.graphics.rectangle("fill", x, y, width, height);
-    love.graphics.setColor(255/255, 0, 0);
-    love.graphics.rectangle("fill", x+width-closeSize, y, closeSize, closeSize);
-    love.graphics.setColor(0, 0, 0);
-    love.graphics.printf("X", x+width-closeSize, y, closeSize, "center");
-    love.graphics.printf(text, x+20, y+20, width-40, "left");
-    love.graphics.setColor(1, 1, 1);
-    function love.mousepressed(mouseX, mouseY, button, isTouch, presses)
-        if mouseX >= x+width-closeSize and mouseX <=x+width and mouseY >= y and mouseY <= y+closeSize then
-           guiObjects:pop();
-        end
-    end
+    end) 
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("escape") then
+        love.event.quit()
+    end
     for i,v in pairs(listOfBullets) do
         v:update(dt);
     end
@@ -141,12 +102,12 @@ function love.draw()
         player:draw();
         world:draw();
     cam:detach()
-    if guiObjects:top() ~= nil then
-        createPopUp(guiObjects:top());
+    if guiStack:top() ~= nil then
+        guiStack:top():draw();
     end
 end
 
-
+    --[[SCRAP CODE]]
     --[[world:setCallbacks(
         function(collider_1, collider_2, contact) 
             print("HERE");
@@ -180,4 +141,26 @@ end
     end, 
     function() 
          
+    end)]]
+
+        --[[enemy.collider:setPostSolve(function(collider_1, collider_2, contact)
+        if collider_1.collision_class == "Enemy" and collider_2.collision_class == "Bullet" then
+            enemy.health = enemy.health - 5;
+            print(enemy.health);
+            for i,v in pairs(listOfBullets) do
+                if v.collider == collider_2 then
+                    v.collider:destroy();
+                    table.remove(listOfBullets, i);
+                end
+            end
+            if enemy.health == 0 then
+                for i,v in pairs(enemys) do
+                    if v == enemy then
+                        table.remove(enemys, i);
+                        print("HERE");
+                    end
+                end
+                enemy.collider:destroy();
+            end
+        end
     end)]]
