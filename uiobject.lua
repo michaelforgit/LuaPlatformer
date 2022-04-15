8ba08ce9d14e26a67a9ea582e65431819e8b137b
michaelforgit <michaelforemail@protonmail.com>
2022-04-15 12:01:29 -0500

    works with the slice class to make 9sliced UI objects

--- uiobject.lua
+++ uiobject.lua
@@ -1,30 +1,34 @@
 UIObject = Object.extend(Object);
-function UIObject:new(specs)
+function UIObject:new(specs)  --(x, y, width, height, closeSize, text)
     self.x = specs[1];
     self.y = specs[2];
+    --self.width = math.max(350, specs[3]);
     self.width = specs[3];
     self.height = specs[4];
     self.closeSize = specs[5];  --Set to 0 if you do not want a close option.
     self.text = specs[6];
     self.scaleX = (love.graphics.getWidth());
     self.scaleY = (love.graphics.getHeight());
+    self.slicedImage = Slice(self.x, self.y, self.width, self.height, 16, "assets/images/red.png")
 end
 
 function UIObject:draw()
     love.graphics.setColor(1, 1, 1);
-    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height);
+    self.slicedImage:draw();
     love.graphics.setColor(0, 0, 0);
     love.graphics.printf(self.text, self.x+20, self.y+20, self.width-40, "left");
     love.graphics.setColor(1, 1, 1);
 end
 
 function UIObject:resize()
+    --love.graphics.setFont(love.graphics.newFont("assets/fonts/Abaddon Light.ttf", 20+(love.graphics.getWidth()/self.scaleX)));
     self.width = self.width * love.graphics.getWidth()/self.scaleX;
     self.height = self.height * love.graphics.getHeight()/self.scaleY;
     self.x = self.x * love.graphics.getWidth()/self.scaleX;
     self.y = self.y * love.graphics.getHeight()/self.scaleY;
     self.scaleY = love.graphics.getHeight();
     self.scaleX = love.graphics.getWidth();
+    self.slicedImage = Slice(self.x, self.y, self.width, self.height, 16, "assets/images/red.png")
 end
 
 function UIObject:update()
